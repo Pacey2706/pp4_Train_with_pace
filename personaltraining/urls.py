fd545920b3212410a3d4a26a63ce8c6d418bf837
Pacey2706 <pacey2706@gmail.com>
2023-07-17 20:29:58 +0000

    add temp html home page and test

--- personaltraining/urls.py
+++ personaltraining/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 
+
+def home(request):
+    return render(request, 'home.html')  # bad practice remove and fix asap
+
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
